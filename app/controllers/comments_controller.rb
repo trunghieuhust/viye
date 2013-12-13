4bde068a13578a71d3263a88689acff88652883a
Trung Hieu <trunghieu.hust@gmail.com>
2013-12-13 14:49:38 +0700

    comment done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(comment_params)
+    @comment.user = current_user
+    @comment.save
     redirect_to @post
   end
           def comment_params
