cc550a9f9345cf041850a33695456c6338cc7ecd
sakaesama <sakaesam.cg@gmail.com>
2013-12-18 02:20:13 +0700

    Layout P01(trang chi tiết post): html, css

--- template/layout_v1/js/layout.js
+++ template/layout_v1/js/layout.js
@@ -1,43 +1,40 @@
 $(document).ready(function(){
 	/*popover login & signup*/
-	$('button.pop-btn').popover({
-			html: true,
-			placement: 'bottom',
-			trigger: 'manual',
-		    title: function () {
-		        return $(this).parent().children('.popover-head:first').html();
-		    },
-		    content: function () {
-		        return $(this).parent().children('.popover-content:first').html();
-		    }
-		}).click(function() {
-			$(this).parent().children('.popover').removeClass('hide');
-			$('button.pop-btn').not(this).parent().children('.popover').addClass('hide');
-			$('button.pop-btn').not(this).popover('hide');
-			$(this).popover('toggle');
-		});
+		$('button.pop-btn').popover({
+				html: true,
+				placement: 'bottom',
+				trigger: 'manual',
+			    title: function () {
+			        return $(this).parent().children('.popover-head:first').html();
+			    },
+			    content: function () {
+			        return $(this).parent().children('.popover-content:first').html();
+			    }
+			}).click(function() {
+				$(this).parent().children('.popover').removeClass('hide');
+				$('button.pop-btn').not(this).parent().children('.popover').addClass('hide');
+				$('button.pop-btn').not(this).popover('hide');
+				$(this).popover('toggle');
+			});
 
-	$('html').click(function (e) {
-	        if (typeof $(e.target).data('original-title') == 'undefined' && !$(e.target).parents().is('.popover.in')) {
-	            $(".pop-btn").popover('hide');
-	        }
-	    });
-
+		$('html').click(function (e) {
+		        if (typeof $(e.target).data('original-title') == 'undefined' && !$(e.target).parents().is('.popover.in')) {
+		            $(".pop-btn").popover('hide');
+		        }
+		    });
 	/*tooltip js*/
-	$("[rel='tooltip']").tooltip();
-	$("[rel='tooltip-bottom']").tooltip({
-		placement: 'bottom'
-	});
-
+		$("[rel='tooltip']").tooltip();
+		$("[rel='tooltip-bottom']").tooltip({
+			placement: 'bottom'
+		});
 	/*scroll js*/
-	$('#description').perfectScrollbar({
-	          wheelSpeed: 20,
-	          wheelPropagation: false
-	});
-
+		$('#description').perfectScrollbar({
+		          wheelSpeed: 20,
+		          wheelPropagation: false
+		});
 	/*UptoTop js*/
 	// Show or hide the sticky footer button
-	$(window).scroll(function() {
+		$(window).scroll(function() {
 		if ($(this).scrollTop() > 200) {
 			$('.go-top').fadeIn(200);
 			} else {
@@ -49,4 +46,10 @@ $(document).ready(function(){
 			event.preventDefault();
 			$('html, body').animate({scrollTop: 0}, 300);
 		})
+	//date picker
+		$(function() {
+		    $('#datetimepicker4').datetimepicker({
+		      	pickTime: false
+		    });
+		});
 });
