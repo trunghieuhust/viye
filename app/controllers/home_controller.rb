6650c17fa97e691938cd0b2695b26119d0cb9251
Trung Hieu <trunghieu.hust@gmail.com>
2013-12-25 15:31:15 +0700

    homepage done, except:booking, maps, tags count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ before_action :authenticate_user!, except: [:index, :show]
   def index
   	@post = Post.all.take(3)
   	@user_posts = Post.all
+  	@admin_posts_mienbac = Post.all
+  	@admin_posts_mientrung = Post.all
+  	@admin_posts_miennam = Post.all
   end
 end
